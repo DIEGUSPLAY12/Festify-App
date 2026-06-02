df18c8bb00be313f59d389f1553aba3d16b9b554
Diego García <diegosenciales@gmail.com>
2026-06-02 19:28:26 +0200

    .

--- nightscore-docs/next.config.ts
+++ nightscore-docs/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  // basePath: '/NOMBRE-DEL-REPOSITORIO', // <-- DESCOMENTA Y CAMBIA ESTO si tu URL en github no es la principal (tu-usuario.github.io) sino que tiene nombre de proyecto (tu-usuario.github.io/festify-docs)
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
