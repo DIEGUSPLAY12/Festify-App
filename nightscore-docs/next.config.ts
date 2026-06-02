217c7d8e5a09cd51ba36aa34655a4e6fe5a3d63c
Diego García <diegosenciales@gmail.com>
2026-06-02 19:31:50 +0200

    Configurar gh-pages

--- nightscore-docs/next.config.ts
+++ nightscore-docs/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/Festify-App',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
